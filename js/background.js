8106c062067cfcb0ef04ac169c3c5b5f4b298909
Chester Guinto <cguinto@rapiddisplays.com>
2020-01-02 17:05:24 -0800

    modified:   css/style.css         -Added styling for image fade.         -Added animations for slideshow style background.
    	modified:   index.html
            -Added project images for slideshow background.
    
    	modified:   js/background.js
            -Deprecated? No longer using JS for slideshow switching.

--- js/background.js
+++ js/background.js
@@ -1,26 +1,53 @@
-var current = 0;
+var imgIndex = 0;
 var gradient = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5))";
+var backgrounds = [
+    "../assets/Corp gift tea web site content/coffee gift sets/simple coffee with a gift box.jpg",
+    "../assets/Corp gift tea web site content/coffee gift sets/simple coffee with travel mug.jpg",
+    "../assets/Corp gift tea web site content/featured projects/code red.jpg",
+    "../assets/Corp gift tea web site content/coffee gift sets/simple coffee with porcelain cup with lid.jpg",
+    "../assets/Corp gift tea web site content/images for small slider/jasmine.jpg",
+    "../assets/Corp gift tea web site content/images for small slider/google.jpg",
+    "../assets/Corp gift tea web site content/tea gift sets boxes/Tea traveler 10.jpg",
+    "../assets/Corp gift tea web site content/tea gift sets boxes/zen.jpg"
+];
 
-window.onload = function(){
+// window.onload = function () {
 
-    function changeImage() {
+//     function changeImage() {
 
-        var backgrounds = [
-            "../assets/Corp gift tea web site content/coffee gift sets/simple coffee with a gift box.jpg",
-            "../assets/Corp gift tea web site content/coffee gift sets/simple coffee with travel mug.jpg",
-            "../assets/Corp gift tea web site content/featured projects/code red.jpg",
-            "../assets/Corp gift tea web site content/coffee gift sets/simple coffee with porcelain cup with lid.jpg",
-            "../assets/Corp gift tea web site content/images for small slider/jasmine.jpg",
-            "../assets/Corp gift tea web site content/images for small slider/google.jpg",
-            "../assets/Corp gift tea web site content/tea gift sets boxes/Tea traveler 10.jpg",
-            "../assets/Corp gift tea web site content/tea gift sets boxes/zen.jpg"
-        ];
+//         document.body.style.background = gradient + ', url("' + backgrounds[imgIndex] + '") no-repeat center center fixed';
+//         document.body.style.backgroundSize = "60%";
 
-        document.body.style.background = gradient + ', url("' + backgrounds[current] + '") no-repeat center center fixed';
-        document.body.style.backgroundSize = "60%";
+//         imgIndex++;
+//         imgIndex = imgIndex % backgrounds.length;
+
         
-        current++;
-        current = current %backgrounds.length;
+//     }
+//     //setInterval(fadeOut, 2000);
+//     window.setInterval(changeImage, 2000);
+//     //setInterval(fadeIn, 2000);
+// }
+
+function fadeOut() {
+    
+    
+    for (var i = 1; i < 100; i += 1) { // change the += 1 for different smoothness
+        (function (i) {
+            setTimeout(function () {
+                document.getElementById("bg").style.opacity = 1 - (i * 0.01);
+            }, i * 10);
+        })(i);
     }
-    window.setInterval(changeImage, 1500);
+
+    
+}
+
+function fadeIn() {
+    for (var i = 1; i < 100; i += 1) { // change the += 1 for different smoothness
+        (function (i) {
+            setTimeout(function () {
+                document.getElementById("bg").style.opacity = i * 0.01;
+            }, i * 10);
+        })(i);
+    }
 }
